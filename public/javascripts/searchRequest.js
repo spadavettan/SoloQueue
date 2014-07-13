cc6066dd0e1fb4e090b5059010af8cd6451b63e1
Sachaniman <sachan.ganesh@gmail.com>
2014-07-12 17:04:03 -0700

    Fix calculation bugs and add switch conditionals

--- public/javascripts/searchRequest.js
+++ public/javascripts/searchRequest.js
@@ -1,5 +1,5 @@
 ﻿var form = $('#search-form');
 var btn = $('#search-request');
 btn.click(function () {
-    window.location = window.location + "/search?name=" + form.val();
+    window.location = window.location + "/search?name=" + (form.val() || "LeeSinSux");
 });
